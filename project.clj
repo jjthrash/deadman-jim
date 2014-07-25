714d8896a9baed67ab09cd4b3145a6edb7bcca06
Jimmy Thrasher <jimmy@jimmythrasher.com>
2014-07-25 17:34:13 -0400

    Try fixing deploy problem by installing an org.codehaus.jackson dependency

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [ring/ring-json "0.3.1"]
                  [ring-json-params "0.1.0"]
                  [org.clojure/data.json "0.2.5"]
-                 [com.twilio.sdk/twilio-java-sdk "3.3.16"]]
+                 [com.twilio.sdk/twilio-java-sdk "3.3.16"]
+                 [org.codehaus.jackson/jackson-core-asl "1.9.13"]]
   :plugins [[lein-ring "0.8.11"]]
   :resource-paths ["lib/*"]
   :min-lein-version "2.0.0")
